e5f3ef48a83961bbca6af307702e018d523c2673
lorena-b <46202743+lorena-b@users.noreply.github.com>
2020-08-06 16:21:16 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = (req,res,next)=>{
+
+    console.log("Middleware Executed")
+    next()
+
+}
+
+app.use(customMiddleware)
+
 app.get("/", (req,res)=>{
+    console.log("home")
     res.send("hello world")
 })
 
+app.get("/about", (req,res)=>{
+    console.log("about")
+    res.send("about page")
+})
+
+
 app.listen(PORT,()=>{
-    console.log("server is running on", PORT)
+    console.log("Server is running on", PORT)
 })
