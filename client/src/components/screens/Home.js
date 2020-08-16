575e13011398033dc70a17aae0b6ad65261fa072
lorena-b <46202743+lorena-b@users.noreply.github.com>
2020-08-16 01:05:59 -0400

    static ui

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,9 +1,56 @@
 import React from 'react';
 
-const Home = ()=>{
+const Home = () => {
 
     return (
-        <h1>Home</h1>
+        <div className="home">
+
+            <div className="card home-card">
+                <h5>Julie</h5>
+                <div className="card-image">
+                    <img alt="landscape" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Post Title</h6>
+                    <p>This is a caption</p>
+                    <input type="text" placeholder="Add a comment" />
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>Julie</h5>
+                <div className="card-image">
+                    <img alt="landscape" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Post Title</h6>
+                    <p>This is a caption</p>
+                    <input type="text" placeholder="Add a comment" />
+                </div>
+
+            </div>
+
+            <div className="card home-card">
+                <h5>Julie</h5>
+                <div className="card-image">
+                    <img alt="landscape" src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                </div>
+
+                <div className="card-content">
+                    <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Post Title</h6>
+                    <p>This is a caption</p>
+                    <input type="text" placeholder="Add a comment" />
+                </div>
+
+            </div>
+
+
+        </div>
 
     )
 
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
-const Profile = ()=>{
+const Profile = () => {
 
     return (
-        <h1>Profile</h1>
+
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"
+                        alt="profileimg" />
+                </div>
+
+                <div>
+                    <h4>Julie Sun</h4>
+                    <div style={{ display: "flex", justifyContent: "space-between", width:"108%" }}>
+                        <h5>40 Posts</h5>
+                        <h5>40 followers</h5>
+                        <h5>40 following</h5>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="gallery">
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+                <img className="item" alt="post" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_ocW9x-t2gGJPKg3iqvoEfm7qJSB9ujqnqA&usqp=CAU"/>
+           
+            </div>
+        </div>
+
+
+
 
     )
 
