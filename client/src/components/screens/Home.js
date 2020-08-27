d9ccc078828a1bd2bdd57cee1f9b01a46865b5f5
lorena-b <46202743+lorena-b@users.noreply.github.com>
2020-08-26 23:46:31 -0400

    delete post

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -100,13 +100,37 @@ const Home = () => {
         })
     }
 
+    const deletePost = (postId)=>{
+        fetch(`/deletepost/${postId}`, {
+            method: "delete",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('jwt')
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData =  data.filter(item=>{
+                return item._id != result._id
+            })
+            setData(newData)
+        })
+    }
+
     return (
         <div className="home">
             {
                 data.map(item => {
                     return (
                         <div className="card home-card" key={item._id}>
-                            <h5>{item.postedBy.name}</h5>
+                            <h5>{item.postedBy.name}
+                                {item.postedBy._id == state._id
+                                && <i className="material-icons"
+                                style = {{float:"right"}}
+                                onClick={()=>deletePost(item._id)}>
+                                delete
+                                </i>}
+                                
+                            </h5>
                             <div className="card-image">
                                 <img alt="landscape" src={item.photo} />
                             </div>
